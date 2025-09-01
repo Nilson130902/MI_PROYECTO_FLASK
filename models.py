905ea249cf22ea27b63f1bfff77531733a08a01c
Nilson López <na.lopezm@uea.edu.ec>
2025-08-31 23:29:42 -0500

    semana 11

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False, default=0)
+    precio = db.Column(db.Float, nullable=False, default=0.0) 
+
+    def __repr__(self):
+        return f'<Producto {self.id} {self.nombre}>'
+
+    def to_tuple(self):
+        return (self.id, self.nombre, self.cantidad, self.precio)
